28ee106144a52a00f44b21b2c96bd064d5cc05fc
hectorvaldiviezo <hvaldiviezos@transportespakatnamu.com>
2025-06-24 17:51:22 -0500

    Create 2025_06_24_141853_add_column_slug_to_config_vista.php

---
+++ database/migrations/2025_06_24_141853_add_column_slug_to_config_vista.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('config_vista', function (Blueprint $table) {
+            $table->string('slug')->nullable()->after('descripcion');
+            $table->string('icon')->nullable()->after('icono');
+            $table->string('route')->nullable()->after('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('config_vista', function (Blueprint $table) {
+            $table->dropColumn(['slug', 'icon', 'route']);
+        });
+    }
+};
