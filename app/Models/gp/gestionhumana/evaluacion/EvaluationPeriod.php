921004d1d1e35478f4145b4fee7a9e95e23d4af8
david3010M <hvaldiviezos@unprg.edu.pe>
2025-07-15 22:44:43 -0500

    feat:  model

--- app/Models/gp/gestionhumana/evaluacion/EvaluationPeriod.php
+++ app/Models/gp/gestionhumana/evaluacion/EvaluationPeriod.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\gp\gestionhumana\evaluacion;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class EvaluationPeriod extends Model
+class EvaluationPeriod extends BaseModel
 {
-    /** @use HasFactory<\Database\Factories\EvaluationPeriodFactory> */
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'gh_evaluation_periods';
+
+    protected $fillable = [
+        'name',
+        'start_date',
+        'end_date',
+        'active',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+        'active' => 'boolean',
+    ];
+
 }
--- database/migrations/2025_07_05_101245_create_evaluation_periods_table.php
+++ database/migrations/2025_07_05_101245_create_evaluation_periods_table.php
@@ -4,16 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('evaluation_periods', function (Blueprint $table) {
+        Schema::create('gh_evaluation_periods', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('active')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('evaluation_periods');
+        Schema::dropIfExists('gh_evaluation_periods');
     }
 };
