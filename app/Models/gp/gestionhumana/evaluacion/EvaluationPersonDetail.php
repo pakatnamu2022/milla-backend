c417bac2ba2b29f41c223a5af1b4a89781519ef0
david3010M <hvaldiviezos@unprg.edu.pe>
2025-08-23 12:48:56 -0500

    Update EvaluationPersonDetail.php

--- app/Models/gp/gestionhumana/evaluacion/EvaluationPersonDetail.php
+++ app/Models/gp/gestionhumana/evaluacion/EvaluationPersonDetail.php
@@ -18,6 +18,6 @@ class EvaluationPersonDetail extends Model
 
   public function person()
   {
-    return $this->hasOne(Person::class, 'person_id');
+    return $this->belongsTo(Person::class, 'person_id');
   }
 }
