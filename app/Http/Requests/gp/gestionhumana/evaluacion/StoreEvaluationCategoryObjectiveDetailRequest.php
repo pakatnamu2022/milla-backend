d448ffe1a634a242d1425a0263af8dfff2272c3f
David Valdiviezo <hvaldiviezos@unprg.edu.pe>
2025-08-27 18:05:59 -0500

    feat: request

--- app/Http/Requests/gp/gestionhumana/evaluacion/StoreEvaluationCategoryObjectiveDetailRequest.php
+++ app/Http/Requests/gp/gestionhumana/evaluacion/StoreEvaluationCategoryObjectiveDetailRequest.php
@@ -3,14 +3,27 @@
 namespace App\Http\Requests\gp\gestionhumana\evaluacion;
 
 use App\Http\Requests\StoreRequest;
+use Illuminate\Validation\Rule;
 
 class StoreEvaluationCategoryObjectiveDetailRequest extends StoreRequest
 {
   public function rules(): array
   {
     return [
-      'objective_id' => 'required|integer|exists:gh_evaluation_objective,id',
-      'category_id' => 'required|integer|exists:gh_hierarchical_category,id',
+      'objective_id' => [
+        'required',
+        'integer',
+        Rule::exists('gh_evaluation_objective', 'id')->whereNull('deleted_at'),
+        Rule::unique('gh_evaluation_category_objective')->where(function ($query) {
+          return $query->where('category_id', $this->category_id)
+            ->whereNull('deleted_at');
+        })
+      ],
+      'category_id' => [
+        'required',
+        'integer',
+        Rule::exists('gh_hierarchical_category', 'id')->whereNull('deleted_at')
+      ]
     ];
   }
 }
