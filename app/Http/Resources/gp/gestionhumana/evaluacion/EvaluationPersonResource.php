17a166d719854694ce390739ded2495147a27918
David Valdiviezo <hvaldiviezos@unprg.edu.pe>
2025-09-18 09:45:19 -0500

    Add range result and label calculation to resource
    Introduces 'index_range_result' and 'label_range' fields to EvaluationPersonResource. These fields are calculated based on whether the position's hierarchical category has objectives, using new helper methods to determine the appropriate index and label for the qualification percentage.

--- app/Http/Resources/gp/gestionhumana/evaluacion/EvaluationPersonResource.php
+++ app/Http/Resources/gp/gestionhumana/evaluacion/EvaluationPersonResource.php
@@ -20,6 +20,32 @@ class EvaluationPersonResource extends JsonResource
       'qualification' => round($this->qualification, 2),
       'comment' => $this->comment,
       'wasEvaluated' => $this->wasEvaluated,
+      'index_range_result' => $this->person->position->hierarchicalCategory->hasObjectives ? $this->calculateIndexRangeResult($this->qualification, $this->evaluation->objectiveParameter) : $this->evaluation->objectiveParameter->details->count() - 1,
+      'label_range' => $this->person->position->hierarchicalCategory->hasObjectives ? $this->calculateLabelRangeResult($this->qualification, $this->evaluation->objectiveParameter) : $this->evaluation->objectiveParameter->details->last()->label,
+
     ];
   }
+
+  private function calculateIndexRangeResult($percentage, $parameter)
+  {
+    $orderedDetailsByTo = $parameter->details->sortBy('to');
+    foreach ($orderedDetailsByTo as $index => $detail) {
+      if ($percentage < $detail->to) {
+        return $index;
+      }
+    }
+    return $orderedDetailsByTo->count() - 1; // Si es mayor que todos, retorna el último índice
+  }
+
+  private function calculateLabelRangeResult($percentage, $parameter)
+  {
+    $orderedDetailsByTo = $parameter->details->sortBy('to');
+    foreach ($orderedDetailsByTo as $detail) {
+      if ($percentage < $detail->to) {
+        return $detail->label;
+      }
+    }
+    return $orderedDetailsByTo->last()->label; // Si es mayor que todos, retorna la última etiqueta
+  }
 }
+
