5da1eb5f7c8730be4c52c240da0b9efaa56bf53d
Wilmer Suclupe Farroñan <124706662+wilmersf2001@users.noreply.github.com>
2025-11-03 22:00:07 -0500

    Remove unused imports from SunatConceptsSeeder
    Cleaned up the SunatConceptsSeeder by removing unused imports: WithoutModelEvents and DB. This helps improve code readability and maintainability.

--- database/seeders/SunatConceptsSeeder.php
+++ database/seeders/SunatConceptsSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use App\Models\gp\maestroGeneral\SunatConcepts;
 
