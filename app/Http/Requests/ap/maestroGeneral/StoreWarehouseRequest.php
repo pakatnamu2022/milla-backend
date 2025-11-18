f750af511df09d71d366ae96b2c80ac4607e483e
Wilmer Suclupe Farroñan <124706662+wilmersf2001@users.noreply.github.com>
2025-11-18 11:45:53 -0500

    Comment out unique validation for dyn_code and description
    The unique validation rules and corresponding error messages for 'dyn_code' and 'description' fields have been commented out in StoreWarehouseRequest. This change may be intended to temporarily disable uniqueness checks for these fields.

--- app/Http/Requests/ap/maestroGeneral/StoreWarehouseRequest.php
+++ app/Http/Requests/ap/maestroGeneral/StoreWarehouseRequest.php
@@ -14,9 +14,9 @@ class StoreWarehouseRequest extends StoreRequest
         'required',
         'string',
         'max:10',
-        Rule::unique('warehouse', 'dyn_code')
-          ->where('article_class_id', $this->article_class_id)
-          ->ignore($this->route('warehouse')),
+//        Rule::unique('warehouse', 'dyn_code')
+//          ->where('article_class_id', $this->article_class_id)
+//          ->ignore($this->route('warehouse')),
       ],
       'description' => [
         'required',
@@ -61,12 +61,12 @@ class StoreWarehouseRequest extends StoreRequest
       'dyn_code.required' => 'El código dynamic es obligatorio.',
       'dyn_code.string' => 'El código dynamic debe ser un texto.',
       'dyn_code.max' => 'El código dynamic no puede tener más de 10 caracteres.',
-      'dyn_code.unique' => 'El código dynamic ya está registrado para la clase de artículo seleccionada.',
+      //'dyn_code.unique' => 'El código dynamic ya está registrado para la clase de artículo seleccionada.',
 
       'description.required' => 'La descripción es obligatoria.',
       'description.string' => 'La descripción debe ser un texto.',
       'description.max' => 'La descripción no puede exceder los 100 caracteres.',
-      'description.unique' => 'La descripción ya está registrada.',
+      //'description.unique' => 'La descripción ya está registrada.',
 
       'sede_id.required' => 'La sede es obligatoria.',
       'sede_id.integer' => 'La sede debe ser un número entero.',
