0d45f181c243dae25f1b7df1ce195da0ad2f9593
David Valdiviezo <hvaldiviezos@unprg.edu.pe>
2026-01-30 15:22:46 -0500

    Update PhoneLineWorkerController.php

--- app/Http/Controllers/gp/tics/PhoneLineWorkerController.php
+++ app/Http/Controllers/gp/tics/PhoneLineWorkerController.php
@@ -10,50 +10,50 @@ use App\Http\Services\gp\tics\PhoneLineWorkerService;
 
 class PhoneLineWorkerController extends Controller
 {
-    protected PhoneLineWorkerService $service;
+  protected PhoneLineWorkerService $service;
 
-    public function __construct(PhoneLineWorkerService $service)
-    {
-        $this->service = $service;
-    }
+  public function __construct(PhoneLineWorkerService $service)
+  {
+    $this->service = $service;
+  }
 
-    public function index(IndexPhoneLineWorkerRequest $request)
-    {
-        return $this->service->list($request);
-    }
+  public function index(IndexPhoneLineWorkerRequest $request)
+  {
+    return $this->service->list($request);
+  }
 
-    public function store(StorePhoneLineWorkerRequest $request)
-    {
-        $data = $request->validated();
-        return response()->json($this->service->store($data));
-    }
+  public function store(StorePhoneLineWorkerRequest $request)
+  {
+    $data = $request->validated();
+    return response()->json($this->service->store($data));
+  }
 
-    public function show($id)
-    {
-        try {
-            return response()->json($this->service->show($id));
-        } catch (\Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 404);
-        }
+  public function show($id)
+  {
+    try {
+      return response()->json($this->service->show($id));
+    } catch (\Exception $e) {
+      return response()->json(['message' => $e->getMessage()], 404);
     }
+  }
 
-    public function update(UpdatePhoneLineWorkerRequest $request, $id)
-    {
-        try {
-            $data = $request->validated();
-            $data['id'] = $id;
-            return response()->json($this->service->update($data));
-        } catch (\Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 404);
-        }
+  public function update(UpdatePhoneLineWorkerRequest $request, $id)
+  {
+    try {
+      $data = $request->validated();
+      $data['id'] = $id;
+      return response()->json($this->service->update($data));
+    } catch (\Exception $e) {
+      return response()->json(['message' => $e->getMessage()], 404);
     }
+  }
 
-    public function destroy($id)
-    {
-        try {
-            return $this->service->destroy($id);
-        } catch (\Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 404);
-        }
+  public function destroy($id)
+  {
+    try {
+      return $this->service->destroy($id);
+    } catch (\Exception $e) {
+      return response()->json(['message' => $e->getMessage()], 404);
     }
+  }
 }
