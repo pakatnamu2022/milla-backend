32e7e5e83e26d29234aac02d33e8e1d8c9f43bed
David Valdiviezo <hvaldiviezos@unprg.edu.pe>
2026-03-20 18:19:50 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,11 @@ class User extends Authenticatable
 
   protected $table = 'usr_users';
 
+  public function getAuthIdentifierName()
+  {
+    return 'username';
+  }
+
   protected $fillable = [
     'id',
     'partner_id',
