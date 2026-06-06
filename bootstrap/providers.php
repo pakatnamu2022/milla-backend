55f62cfacf837437d537cbf74b110102b61567a5
david3010M <hvaldiviezos@unprg.edu.pe>
2026-06-06 09:40:45 -0500

    Enable HorizonServiceProvider
    Uncomment and implement App\Providers\HorizonServiceProvider: restore namespace, imports, class, boot() and gate() methods (gate currently allows all users by returning true). Also enable the provider in bootstrap/providers.php by uncommenting App\Providers\HorizonServiceProvider::class so Laravel will register Horizon services.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CustomValidatorServiceProvider::class,
-    //App\Providers\HorizonServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     Barryvdh\DomPDF\ServiceProvider::class,
 ];
