480797b8863d1f967eb18df765c0356bfd2fc02c
david3010M <hvaldiviezos@unprg.edu.pe>
2026-07-07 18:48:01 -0500

    Update Vehicles.php

--- app/Models/ap/comercial/Vehicles.php
+++ app/Models/ap/comercial/Vehicles.php
@@ -91,7 +91,7 @@ class Vehicles extends BaseModel
    */
   public function getIsReceivedAttribute()
   {
-    return true;
+    return false;
     return $this->shippingGuides()->where('document_type', ShippingGuides::DOCUMENT_TYPE_GR)->where('')
       ->orWhere('document_number', 'like', 'SI-%')->exists();
   }
