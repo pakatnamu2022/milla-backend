b602fdce39b0415291ccf1ed0d7ddcb60bc6d6ff
david3010M <hvaldiviezos@unprg.edu.pe>
2026-08-11 10:36:40 -0500

    bulk permissions

--- app/Http/Requests/gp/gestionsistema/StoreMultiplePermissionRoleRequest.php
+++ app/Http/Requests/gp/gestionsistema/StoreMultiplePermissionRoleRequest.php
@@ -3,18 +3,16 @@
 namespace App\Http\Requests\gp\gestionsistema;
 
 use App\Http\Requests\StoreRequest;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class StoreMultiplePermissionRoleRequest extends StoreRequest
 {
   public function rules(): array
   {
     return [
-      'role_id' => 'required|exists:config_roles,id',
-      'permissions' => 'nullable|array',
-      'permissions.*' => 'exists:permission,id',
-      'permissions_to_remove' => 'nullable|array',
+      'role_id'                 => 'required|exists:config_roles,id',
+      'permissions'             => 'nullable|array',
+      'permissions.*'           => 'exists:permission,id',
+      'permissions_to_remove'   => 'nullable|array',
       'permissions_to_remove.*' => 'exists:permission,id',
     ];
   }
@@ -25,8 +23,8 @@ class StoreMultiplePermissionRoleRequest extends StoreRequest
   public function attributes(): array
   {
     return [
-      'role_id' => 'rol',
-      'permissions' => 'permisos',
+      'role_id'               => 'rol',
+      'permissions'           => 'permisos',
       'permissions_to_remove' => 'permisos a eliminar',
     ];
   }
