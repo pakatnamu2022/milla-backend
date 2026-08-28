8c785988761da9974ec0e104c17fc74773c29c99
Wilmer Suclupe Farroñan <124706662+wilmersf2001@users.noreply.github.com>
2026-08-28 18:33:17 -0500

    validacion de boleras realizada para migracion a dynamics

--- routes/api.php
+++ routes/api.php
@@ -1658,6 +1658,7 @@ Route::middleware(['auth:sanctum'])->group(callback: function () {
       Route::post('inventoryMovements/transfers/{id}/cancel', [InventoryMovementController::class, 'cancelTransfer']);
       Route::delete('inventoryMovements/transfers/{id}', [InventoryMovementController::class, 'destroyTransfer']);
       Route::get('inventoryMovements/kardex', [InventoryMovementController::class, 'getKardex']);
+      Route::get('inventoryMovements/kardex/export', [InventoryMovementController::class, 'exportKardex']);
       Route::get('inventoryMovements/product/{productId}/warehouse/{warehouseId}/history', [InventoryMovementController::class, 'getProductMovementHistory']);
       Route::get('inventoryMovements/product/{productId}/warehouse/{warehouseId}/purchase-history', [InventoryMovementController::class, 'getProductPurchaseHistory']);
       Route::get('inventoryMovements/product/{productId}/warehouse/{warehouseId}/history/export', [InventoryMovementController::class, 'exportProductMovementHistory']);
