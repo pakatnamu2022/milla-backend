00fe719d15b78e8fcd4d690cfa955e64c6ab120b
Wilmer Suclupe Farroñan <124706662+wilmersf2001@users.noreply.github.com>
2026-09-22 16:39:12 -0500

    Update ElectronicDocumentService.php

--- app/Http/Services/ap/facturacion/ElectronicDocumentService.php
+++ app/Http/Services/ap/facturacion/ElectronicDocumentService.php
@@ -4520,7 +4520,7 @@ class ElectronicDocumentService extends BaseService implements BaseServiceInterf
     // NO actualizar para: Notas de Crédito ni Anticipos
     // Se actualiza con la fecha de emisión del documento electrónico
     if ($document->sunat_concept_document_type_id != ElectronicDocument::TYPE_NOTA_CREDITO
-        && !$document->is_advance_payment) {
+      && !$document->is_advance_payment) {
       $workOrder->updateOfficialClosingDate($document->fecha_de_emision);
     }
   }
@@ -4817,7 +4817,7 @@ class ElectronicDocumentService extends BaseService implements BaseServiceInterf
           'total' => $itemTotal,
           'account_plan_id' => $item['account_plan_id'] ?? null,
           'product_id' => $item['product_id'] ?? null,
-          'is_traverse' => $item['is_traverse'] ?? null,
+          'is_traverse' => $item['is_traverse'] ?? false,
         ]);
       }
 
